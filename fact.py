9aebbbd36e57da839ed2d8eae42178c3d2048923
shriya-27-s <shriyashukla2027@gmail.com>
2026-02-11 22:21:24 +0530

    Add files via upload

---
+++ fact.py
@@ -0,0 +1,15 @@
+'''
+Problem Statement: Write a Python program that:
+1.   Defines a function named factorial that takes a number as an argument and
+     calculates its factorial using a loop or recursion.
+2.   Returns the calculated factorial.
+3.   Calls the function with a sample number and prints the output.
+'''
+
+def factorial(num):
+    if num==0:
+        return 1
+    else:
+        return num*factorial(num-1)
+a=int(input("Enter a number: "))
+print(f"factorial of {a} is {factorial(a)}")
